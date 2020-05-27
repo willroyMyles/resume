3ee8385d11d6cd93246177f32c194be40ee7ecaf
Willroy Myles <willroymyles@yahoo.com>
2020-05-27 06:47:21 -0500

    Updates

---
+++ precache-manifest.234cf5d0e8d806bb30d0b4b396f64305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b1ded47f5fc9e6988917a659293972",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "771c29c64871c892af58",
+    "url": "/resume/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c857f56b8645009f36be",
+    "url": "/resume/static/css/main.0ba74034.chunk.css"
+  },
+  {
+    "revision": "771c29c64871c892af58",
+    "url": "/resume/static/js/2.d226ec7d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/resume/static/js/2.d226ec7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c857f56b8645009f36be",
+    "url": "/resume/static/js/main.8ab17d33.chunk.js"
+  },
+  {
+    "revision": "e0f7dc91a34c7427a62d",
+    "url": "/resume/static/js/runtime-main.a54ac2ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.55f4a52962c7802043afca082cbfd961.js"
+  "/resume/precache-manifest.234cf5d0e8d806bb30d0b4b396f64305.js"
 );
 
 self.addEventListener('message', (event) => {
